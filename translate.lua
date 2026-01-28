13f5e0fc610f1be2be65cf749896eacd8ef004c7
shellprompt <aft.ridwan@gmail.com>
2026-01-28 00:20:00 +0000

    Update translate.lua
    added translate function, improved chat bubble stuff, removed chat bubble colour

--- translate.lua
+++ translate.lua
@@ -1,7 +1,10 @@
 local HttpService: HttpService = game:GetService("HttpService")
 local CoreGui: CoreGui = game:GetService("CoreGui")
 local RunService: RunService = game:GetService("RunService")
+local TextChatService: TextChatService = game:GetService("TextChatService")
+local Players: Players = game:GetService("Players")
 
+local localPlayer = game.Players.LocalPlayer
 -- [[ THESE MAY CHANGE IN THE FUTURE. CREATE AN ISSUE IF IT HAS ]] --
 local ChatContainer: Frame = CoreGui.ExperienceChat.appLayout.chatWindow.scrollingView.bottomLockedScrollView.RCTScrollView.RCTScrollContentView
 local BubbleChatContainer: Frame = CoreGui.ExperienceChat.bubbleChat
@@ -14,6 +17,11 @@ local function splitRichText(original: string)
     local prefix, message = original:match("^(.*</font>%s*)(.*)$")
     return prefix, message
 end
+
+local function SendChatMessage(message)
+    local textChannel = TextChatService.TextChannels.RBXGeneral
+    textChannel:SendAsync(message)
+end 
 
 local translatedCache = {}
 
@@ -62,6 +70,43 @@ local function translateMessage(text: string): string
     end
 end
 
+
+local function translateCommand(text: string, target_lang: string): string
+    if not target_lang or not text then return end
+
+    local url: string = string.format("%s/translate?sl=auto&dl=%s&text=%s", base_url, target_lang, text)
+
+    local resp = request({
+        Url = url,
+        Method = "GET",
+    })
+
+    local body: string = resp.Body
+
+    if translatedCache[text] then
+        return translatedCache[text]
+    end
+
+    -- [[ JSON RESPONSE CLEANING. REQUIRED ]] --
+    if body:sub(1,3) == "\239\187\191" then
+        body = body:sub(4)
+    end
+
+    body = body:gsub("^%s+", ""):gsub("%s+$", "")
+    ----------------------------------
+
+    local success, result = pcall(function()
+        return game:GetService("HttpService"):JSONDecode(body)
+    end)
+
+    if not success then
+        warn("Parsing Failed.")
+        return text
+    end
+
+    return result['destination-text']
+end
+
 ChatContainer.ChildAdded:Connect(function(frame)
     if not frame:IsA("Frame") then return end
     if not frame.TextMessage:WaitForChild("PrefixText", 1) then return end
@@ -86,31 +131,54 @@ ChatContainer.ChildAdded:Connect(function(frame)
 end)
 
 BubbleChatContainer.DescendantAdded:Connect(function(frame: Frame)
-    if frame.Name ~= "ChatBubbleFrame" then return end
+    if frame.Name ~= "ChatBubbleFrame" then
+        return
+    end
+
+    local billboard: BillboardGui = frame:FindFirstAncestorOfClass("BillboardGui")
+    if not billboard then
+        return
+    end 
+
+    if not string.match(billboard.Name, "^BubbleChat_%d+$") then
+        if not billboard.Adornee then return end
+        local plr: Player = Players:GetPlayerFromCharacter(billboard.Adornee.Parent)
+        if plr then
+            frame.Parent.Parent = BubbleChatContainer:FindFirstChild(string.format("BubbleChat_%s", tostring(plr.UserId))):FindFirstChild("BubbleChatList")
+        end
+        return
+    end
 
     local msg: string = frame:FindFirstChild("Text").Text
 
     task.spawn(function()
         local translated: string = translateMessage(msg)
         if not translated or msg == translated then return end
-
-        local textLabel = frame:FindFirstChild("Text")
+        local textLabel: TextLabel = frame:FindFirstChild("Text")
         if not textLabel then return end
 
         
-        -- [[ RESCALING CHAT BUBBLE ]] --
-        textLabel.TextWrapped = true
-        textLabel.AutomaticSize = Enum.AutomaticSize.None
-
-        local MAX_WIDTH = 320
-        local PADDING_X = 28
-        local PADDING_Y = 18
-
-        textLabel.Text = translated
-        frame.BackgroundColor3 = Color3.fromRGB(226, 208, 255)
-
-        local bounds = textLabel.TextBounds
-        frame.Size = UDim2.fromOffset(math.min(bounds.X + PADDING_X, MAX_WIDTH), bounds.Y + PADDING_Y)
+        TextChatService:DisplayBubble(billboard.Adornee, translated)
+        frame.Parent:Destroy()
     end)
+end)
 
+localPlayer.Chatted:Connect(function(message: string)
+    local args: {string} = string.split(message:lower(), " ")
+
+    if args[1] == "/e" and args[2] == "translate" then
+        local target_lang: string = args[3]
+        if not target_lang then return end
+        
+        local text = table.concat(args, " ", 4)
+
+        if text == "" then return end
+        TextChatService.TextChannels.RBXGeneral:DisplaySystemMessage(string.format("Translating message: %s", text))
+
+        local translated: string = translateCommand(text, target_lang)
+
+        TextChatService.TextChannels.RBXGeneral:DisplaySystemMessage(string.format("Translated message: %s", translated))
+
+        SendChatMessage(translated)
+    end
 end)
